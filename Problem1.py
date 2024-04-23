e736477882abc52d625b9bb39565670689e94c2e
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-23 16:59:40 +0800

    Open the input and output files

--- Problem1.py
+++ Problem1.py
@@ -3,5 +3,9 @@
 #Create a text file named "numbers.txt"
 
 #Open the input file
+with open('numbers.txt', 'r') as my_file:
+    numbers = my_file.read().split()
+
 #Open the output files
-#Display results
+with open('even.txt', 'w') as even_file, open('odd.txt', 'w') as odd_file:
+
