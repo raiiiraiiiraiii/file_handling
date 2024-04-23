64c7181af42fcead431c757c648a5e77bc88dc72
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-23 17:03:18 +0800

    Sorted the even and odd numbers; created separate txt files

--- Problem1.py
+++ Problem1.py
@@ -8,4 +8,12 @@ with open('numbers.txt', 'r') as my_file:
 
 #Open the output files
 with open('even.txt', 'w') as even_file, open('odd.txt', 'w') as odd_file:
-
+    #Create a loop
+    for num in numbers:
+        # Check if the number is even or odd
+        if int(num) % 2 == 0:
+            # Write even numbers to even.txt
+            even_file.write(num + '\n')
+        else:
+            # Write odd numbers to odd.txt
+            odd_file.write(num + '\n')
