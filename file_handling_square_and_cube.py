6e6a520af016147076de3119ce1b2f330c459c00
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-25 15:56:30 +0800

    Added Effects

--- file_handling_square_and_cube.py
+++ file_handling_square_and_cube.py
@@ -1,5 +1,8 @@
 #Pseudocode
 
+import time
+import sys
+
 #Open source file (integers.txt) to read
 with open('integers.txt', 'r') as source_file:
     # Read the integers from the source file
@@ -16,3 +19,15 @@ with open('double.txt', 'w') as double_file, open('triple.txt', 'w') as triple_f
             # Write the cube of odd numbers to 'triple.txt'
             triple_file.write(str(num ** 3) + '\n')
 #Message to confirm the success of the program
+def slow_print(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.07)  # Adjust the sleep time for faster or slower printing
+
+def print_end():
+    slow_print("\033[1;33;40m")  # Set text color to yellow
+    slow_print("The files double.txt and triple.txt has been created successfully🎉")
+    slow_print("\033[0;0m")  # Reset text color
+
+print_end()
