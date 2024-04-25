4685b5f6ec441b1e1d21796eeefaaa508789d00a
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-25 15:53:14 +0800

    Write the Program and Fill In the integers.txt

--- file_handling_square_and_cube.py
+++ file_handling_square_and_cube.py
@@ -1,6 +1,18 @@
 #Pseudocode
 
 #Open source file (integers.txt) to read
+with open('integers.txt', 'r') as source_file:
+    # Read the integers from the source file
+    integers = [int(line.strip()) for line in source_file.readlines()]
 
 #Open output files (double.txt and triple.txt) for writing
+with open('double.txt', 'w') as double_file, open('triple.txt', 'w') as triple_file:
+    for num in integers:
+        # Check if the number is even or odd
+        if num % 2 == 0:
+            # Write the square of even numbers to 'double.txt'
+            double_file.write(str(num ** 2) + '\n')
+        else:
+            # Write the cube of odd numbers to 'triple.txt'
+            triple_file.write(str(num ** 3) + '\n')
 #Message to confirm the success of the program
