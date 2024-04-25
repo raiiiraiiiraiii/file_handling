14a2a889100506a0bc97799dcb7394695b03e5ca
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-25 16:58:19 +0800

    Minor Changes (2)

--- P1_reading_and_writing_txtfiles/file_handling_reading_and_writing_txtfiles.py
+++ P1_reading_and_writing_txtfiles/file_handling_reading_and_writing_txtfiles.py
@@ -1,5 +1,8 @@
 #Pseudocode
 
+import time
+import sys
+
 #Create a text file named "numbers.txt"
 
 #Open the input file
@@ -7,7 +10,8 @@ with open('numbers.txt', 'r') as my_file:
     numbers = my_file.read().split()
 
 #Open the output files
-with open('even.txt', 'w') as even_file, open('odd.txt', 'w') as odd_file:
+with (open('even.txt', 'w') as even_file,
+      open('odd.txt', 'w') as odd_file):
     #Create a loop
     for num in numbers:
         # Check if the number is even or odd
@@ -18,4 +22,19 @@ with open('even.txt', 'w') as even_file, open('odd.txt', 'w') as odd_file:
             # Write odd numbers to odd.txt
             odd_file.write(num + '\n')
 
-print("Files 'even.txt' and 'odd.txt' have been created successfully.")
+#Add effect on Congratulating the highest student
+def slow_print(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.07)  # Adjust the sleep time for faster or slower printing
+
+def print_end():
+    message = "Files 'even.txt' and 'odd.txt' have been created successfully."
+    slow_print("\033[1;33;40m")  # Set text color to yellow
+    slow_print(message.center(75))
+    slow_print("\033[0;0m")  # Reset text color
+    print()
+    print()
+
+print_end()
