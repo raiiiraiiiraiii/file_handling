e4d77588adb980b7c14e448054895e950ee3dfd2
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-25 14:20:31 +0800

    Write the program

--- file_handling_enter_line.py
+++ file_handling_enter_line.py
@@ -1,7 +1,23 @@
 #Add Pseudocode
 
 #Create an empty list
+lines = []
+
 #Start an infinite loop
+while True:
+    line = input("Enter line: ")
+    lines.append(line)
+    choice = input("Are there more lines y/n? ").lower()
+
 #Validate the user's input
+    while choice not in ['y', 'n']:
+        print("Invalid input. Please enter 'y' or 'n'.")
+        choice = input("Are there more lines y/n? ").lower()
+    if choice == 'n':
+        break
 #Open txt file
+with open("mylife.txt", "w") as file:
+    for line in lines:
+        file.write(line + "\n")
 #End the program
+print("Text contents have been written to mylife.txt")
