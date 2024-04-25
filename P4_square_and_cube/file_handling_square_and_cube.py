c688cbdaeca1d73b10d1fc873ddb7371a56d6869
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-04-25 16:53:45 +0800

    Minor Changes

--- P4_square_and_cube/file_handling_square_and_cube.py
+++ P4_square_and_cube/file_handling_square_and_cube.py
@@ -6,10 +6,12 @@ import sys
 #Open source file (integers.txt) to read
 with open('integers.txt', 'r') as source_file:
     # Read the integers from the source file
-    integers = [int(line.strip()) for line in source_file.readlines()]
+    integers = [int(line.strip())
+                for line in source_file.readlines()]
 
 #Open output files (double.txt and triple.txt) for writing
-with open('double.txt', 'w') as double_file, open('triple.txt', 'w') as triple_file:
+with (open('double.txt', 'w') as double_file,
+      open('triple.txt', 'w') as triple_file):
     for num in integers:
         # Check if the number is even or odd
         if num % 2 == 0:
